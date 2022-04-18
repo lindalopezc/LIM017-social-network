678478bf929a618c349ebe827c8808c00ace97d1
lindalopezc <lindalopezramos61@gmail.com>
2022-04-18 08:33:22 -0500

    Implementación de likes

--- src/templates/Posts.js
+++ src/templates/Posts.js
@@ -1,3 +1,4 @@
+/* eslint-disable max-len */
 /* eslint-disable import/no-cycle */
 import { Modal } from './modal.js';
 import { getUserLocalStorage } from '../firebase/authentication.js';
@@ -34,34 +35,47 @@ export const templatePosts = (doc) => {
                 <p>Estado</p>
                 <p>${doc.data().Estado}</p>
                 <div class="div-likes">
-                <button class = "btn-like-post"  ><img  class = "icon-like-post" src="../img/heart.png"></button>
-                <p class="counter-likes"></p>
+                <button class = "btn-like-post"></button>
+                <p class="counter-likes">${doc.data().Likes.length}</p>
                 </div>
               </div>
             </div>
           </div>`;
   sectionPost.innerHTML = template;
   const btnLike = sectionPost.querySelector('.btn-like-post');
-  let counterLikes = sectionPost.querySelector('.counter-likes');
+  const counterLikes = sectionPost.querySelector('.counter-likes');
   const userData = getUserLocalStorage();
-  let arrayLikes = doc.data().Likes;
-  console.log(arrayLikes);
+  const arrayLikes = doc.data().Likes;
   let arrayLength = arrayLikes.length;
-  if (arrayLength === 0) {
+
+  // Primero vemos si no tiene likes, hacemos que se oculte el contador
+  if (doc.data().Likes.length === 0) {
     counterLikes.style.display = 'none';
   }
-  counterLikes.innerHTML = arrayLength;
 
+  // Aquí le asignamos la clase que contiene el color rojo del like
+  // en caso el id del user se encuentre en el array 'Links' de ese post.
+  if (arrayLikes.includes(userData.uid)) {
+    btnLike.classList.add('icon-like-red');
+  }
+
+  // Evento para dar like y quitar like al post:
   btnLike.addEventListener('click', () => {
-    if (arrayLikes.includes(userData.uid)) {
-      counterLikes.innerHTML = arrayLength - 1;
-      removeLikes(doc.id, userData.uid);
+    // Si el ícono de like está de color rojo, quiere decir que el usuario si le gustaba el post
+    if (btnLike.classList.contains('icon-like-red')) {
+      btnLike.classList.toggle('icon-like-red'); // Hacemos que cambie de color al quitarle la clase.
+      arrayLength -= 1;
+      removeLikes(doc.id, userData.uid); // Llamamos a la función que se encarla de eliminar el id del user que ya no le gusta el post
     } else {
-      counterLikes.innerHTML = arrayLength + 1;
-      counterLikes.style.display = 'block';
-      addLikes(doc.id, userData.uid);
+      btnLike.classList.add('icon-like-red'); // Añadimos la clase para darle color rojo al corazón
+      arrayLength += 1;
+      counterLikes.style.display = 'block'; // Esto lo ponemos en caso, no haya tenido antes likes
+      addLikes(doc.id, userData.uid); // Actualizamos el post, añadiendo el id del usuario que ha dado like al array 'Likes'.
     }
+    // Cambiamos el valor del contador:
+    counterLikes.innerText = arrayLength;
   });
+
   const iconContact = sectionPost.querySelector('.icon-contact-post');
   iconContact.addEventListener('click', () => {
     const root = document.getElementById('root');
